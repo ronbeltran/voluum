84ba7313abdca6d15ecaf8f93c3bf47bec6efe17
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-09 11:45:23 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='voluum',
+      version='0.1',
+      description='Python Client for Voluum API',
+      author='Ronnie Beltran',
+      author_email='rbbeltran.09@gmail.com',
+      url='https://github.com/ronbeltran/voluum',
+      packages=['voluum'],
+      include_package_data=True,
+      install_requires=['requests'],
+      )
