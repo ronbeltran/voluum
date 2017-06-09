8b17ffaa4a394641e1888dbfdaf3d50d4e02a734
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-09 17:38:57 +0800

    Make build_query_str method resilient

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from voluum.utils import build_columns_query_str
+from voluum.utils import build_query_str
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -8,7 +8,15 @@ class UtilsTestCase(unittest.TestCase):
     def setUp(self):
         self.columns = ['visits', 'clicks', 'conversions']
 
-    def test_build_columns_query_str(self):
-        result = build_columns_query_str(self.columns)
+    def test_build_query_str(self):
+        result = build_query_str(self.columns)
         final_result = 'columns=visits&columns=clicks&columns=conversions'
         self.assertEqual(result, final_result)
+
+    def test_build_query_str_invalid_values(self):
+        result = build_query_str(None)
+        self.assertEqual(result, '')
+        result = build_query_str(True)
+        self.assertEqual(result, '')
+        result = build_query_str([1, True, {}])
+        self.assertIsNotNone(result)
--- voluum/utils.py
+++ voluum/utils.py
@@ -1,9 +1,15 @@
-def build_columns_query_str(columns):
+from urllib.parse import quote_plus
+
+
+def build_query_str(columns):
     """
     columns is a list of column name
     columns = ['foo', 'bar', 'baz']
     returns:
     "columns=foo&columns=bar&columns=baz"
     """
-    query_str = map(lambda x: 'columns={}'.format(x), columns)
+    if not isinstance(columns, (list, tuple)):
+        return ''
+    query_str = map(lambda x: 'columns={}'.format(
+                    quote_plus(str(x).encode('utf-8'))), columns)
     return '&'.join(query_str)
