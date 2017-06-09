13c23a44182120caf71b13cd2fa1648b64714e44
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-09 14:58:52 +0800

    Tests for security module

--- tests/test_security.py
+++ tests/test_security.py
@@ -23,3 +23,39 @@ class SecurityTestCase(unittest.TestCase):
         with self.assertRaises(SecurityException):
             resp = self.security2.get_token()
             self.assertIsNotNone(resp)
+
+    def test_get_session(self):
+        resp = self.security.get_token()
+        self.assertIsNotNone(resp)
+        self.assertIn('token', resp)
+        self.assertIn('inaugural', resp)
+        self.assertIn('expirationTimestamp', resp)
+
+        resp2 = self.security.get_session(resp['token'])
+        self.assertIsNotNone(resp2)
+        self.assertIn('alive', resp2)
+        self.assertTrue(resp2['alive'])
+
+    def test_delete_session(self):
+        # create session
+        resp = self.security.get_token()
+        self.assertIsNotNone(resp)
+        self.assertIn('token', resp)
+        self.assertIn('inaugural', resp)
+        self.assertIn('expirationTimestamp', resp)
+
+        # get session
+        resp2 = self.security.get_session(resp['token'])
+        self.assertIsNotNone(resp2)
+        self.assertIn('alive', resp2)
+        self.assertTrue(resp2['alive'])
+
+        # delete session
+        resp3 = self.security.delete_session(resp['token'])
+        self.assertEqual(resp3, '')
+
+        # verify invalidated session
+        resp4 = self.security.get_session(resp['token'])
+        self.assertIsNotNone(resp4)
+        self.assertIn('alive', resp4)
+        self.assertFalse(resp4['alive'])
