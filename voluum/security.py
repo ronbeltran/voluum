d416eee6558b52f49ca0c1d66255589c9d298e5f
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-09 14:59:22 +0800

    Implement security endpoints

--- voluum/security.py
+++ voluum/security.py
@@ -17,24 +17,63 @@ class Security:
         self.email = email
         self.password = password
 
-    def get_token(self):
-        from . import VOLUUM_API
-
-        url = VOLUUM_API + '/auth/session'
-
-        headers = {
+    def headers(self):
+        return {
             'Content-Type': 'application/json; charset=utf-8',
             'Accept': 'application/json',
         }
+
+    def get_token(self):
+        """ POST /auth/session """
+        from . import VOLUUM_API
+
+        url = VOLUUM_API + '/auth/session'
 
         data = {
             'email': self.email,
             'password': self.password,
         }
 
-        resp = requests.post(url, data=json.dumps(data), headers=headers)
+        resp = requests.post(
+            url, data=json.dumps(data), headers=self.headers())
 
         if resp.status_code != 200:
             raise SecurityException(resp.status_code, resp.text)
 
         return resp.json()
+
+    def get_session(self, token):
+        """ GET /auth/session """
+        from . import VOLUUM_API
+
+        url = VOLUUM_API + '/auth/session'
+
+        headers = self.headers()
+        headers.update({
+            'cwauth-token': token,
+        })
+
+        resp = requests.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise SecurityException(resp.status_code, resp.text)
+
+        return resp.json()
+
+    def delete_session(self, token):
+        """ DELETE /auth/session """
+        from . import VOLUUM_API
+
+        url = VOLUUM_API + '/auth/session'
+
+        headers = self.headers()
+        headers.update({
+            'cwauth-token': token,
+        })
+
+        resp = requests.delete(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise SecurityException(resp.status_code, resp.text)
+
+        return resp.text
