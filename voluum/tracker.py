219d44d6cf94dc1397723d03e32b54a3c71146be
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-15 19:01:03 +0800

    Implement tracker module

--- voluum/tracker.py
+++ voluum/tracker.py
@@ -2,7 +2,7 @@ from voluum.utils import VoluumException
 from voluum.utils import fetch
 
 
-class Reports:
+class Tracker:
 
     def __init__(self, token):
         self.token = token
@@ -22,6 +22,33 @@ class Reports:
         url = VOLUUM_API + '/campaign/' + campaign_id
 
         resp = fetch('GET', url, headers=self.headers())
+
+        if resp.status_code != 200:
+            raise VoluumException(resp.status_code, resp.text)
+
+        return resp.json()
+
+    def get_affiliate_networks(self, include_deleted=None, fields=None):
+        """
+        GET /affiliate-network
+        """
+        from . import VOLUUM_API
+
+        url = VOLUUM_API + '/affiliate-network'
+
+        if fields is None:
+            fields = ''
+        elif isinstance(fields, (str, unicode)):
+            fields = fields.strip().split(',')
+
+        params = {
+            'fields': fields,
+        }
+
+        if include_deleted is not None:
+            params.update({'includeDeleted': bool(include_deleted)})
+
+        resp = fetch('GET', url, params=params, headers=self.headers())
 
         if resp.status_code != 200:
             raise VoluumException(resp.status_code, resp.text)
