5e0cf79c416d6742e6025c0a21a589834a3637c5
Ronnie Beltran <rbbeltran.09@gmail.com>
2017-06-19 14:07:00 +0800

    Implement Voluum class

--- voluum/__init__.py
+++ voluum/__init__.py
@@ -1,5 +1,7 @@
 from .security import Security
 from .reports import Reports
+from .tracker import Tracker
+from .utils import VoluumException
 
 VOLUUM_API = 'https://api.voluum.com'
 
@@ -7,6 +9,23 @@ VOLUUM_API = 'https://api.voluum.com'
 class Voluum:
     def __init__(self, email, password):
         self.email = email
-        self._security = Security(email, password)
-        self.token = self._security.get_token()
-        self._reports = Reports(self.token)
+        self.password = password
+        self.security = None
+        self.reports = None
+        self.tracker = None
+        self.token_json = None
+        self.token = None
+        self.headers = None
+
+    def login(self):
+        self.security = Security(self.email, self.password)
+        resp = self.security.get_token()
+
+        if resp.status_code == 200:
+            self.token_json = resp.json()
+            self.token = self.token_json['token']
+            self.reports = Reports(self.token)
+            self.tracker = Tracker(self.token)
+            self.headers = {'cwauth-token': self.token}
+        else:
+            raise VoluumException(resp.status_code, resp.text)
